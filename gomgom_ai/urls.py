e210ef7ccf65b13670acbe4bc55add529f8f9373
feed-mina <myelin24@naver.com>
2025-04-25 23:47:55 +0900

    워드프레스 경로 urlpatterns에서 막음

--- gomgom_ai/urls.py
+++ gomgom_ai/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from gomgom_ai import views
+from django.http import HttpResponseNotFound
 
+def skip_wordpress(request):
+    return HttpResponseNotFound("")
 urlpatterns = [
+    path('blog/', skip_wordpress),
     path('', views.main, name='main'),
     path('home/', views.home_view, name='home'),
     # 입맛 테스트
