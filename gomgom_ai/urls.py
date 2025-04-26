e310b13b2379afe934d16bed8908163e1780e18a
feed-mina <myelin24@naver.com>
2025-04-26 19:12:58 +0900

    adminSetting

--- gomgom_ai/urls.py
+++ gomgom_ai/urls.py
@@ -4,15 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from gomgom_ai import views
 from django.http import HttpResponseNotFound
+from django.contrib import admin
 
 def skip_wordpress(request):
     return HttpResponseNotFound("")
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('blog/', skip_wordpress),
     path('', views.main, name='main'),
     path('home/', views.home_view, name='home'),
-    # 입맛 테스트
-    path('start/', views.start_view, name='start'),
+    path('start/', views.start_view, name='start'),  # 입맛 테스트
     path('test/', views.test_view, name='test'),
     path('test_result/', views.test_result_view, name='test_result'),
     path('recommend_result/', views.recommend_result, name='recommend_result'),
